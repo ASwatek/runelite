80ed48b87765344d75a3e945013e30ec09c5e73d
ASwatek <52114510+ASwatek@users.noreply.github.com>
2020-04-30 19:05:10 +0200

    back to it?
    Signed-off-by: ASwatek <52114510+ASwatek@users.noreply.github.com>

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/theatre/src/main/java/net/runelite/client/plugins/theatre/timers/Timeable.java
@@ -0,0 +1,8 @@
+package net.runelite.client.plugins.theatre.src.main.java.net.runelite.client.plugins.theatre.timers;
+
+import java.util.Map;
+
+public interface Timeable
+{
+	Map<String, Long> getTimes();
+}
